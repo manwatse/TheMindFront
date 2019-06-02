b94a1815ec245ff2dd21bca023943e336c7cc9f5
manwa <manwa.tse@student.fontys.nl>
2019-06-02 14:34:37 +0200

    firebase works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, Title} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Reactive Form
@@ -42,7 +42,7 @@ import { AuthService } from './shared/services/auth.service';
     AngularFirestoreModule,
     ReactiveFormsModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, Title],
   bootstrap: [AppComponent]
 })
 
